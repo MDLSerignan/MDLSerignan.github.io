b58db53ccd04681cb40b4361eea30ec29d6e1e1b
Centaurus-dj <53085471+Centaurus-dj@users.noreply.github.com>
2020-07-01 15:16:45 +0200

    remade userlist

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
+
+
+userslist = [
+  {
+    usrname: "admin", pswd: "linux"
+  },
+  {
+    usrname: "RobinGarcia", pswd: "1hD7lkP#"
+  },
+  {
+    usrname: "LucieDumaire", pswd: "2hD7lkP#"
+  },
+  {
+    usrname: "MatheoRicco", pswd: "3hD7lkP#"
+  },
+  {
+    usrname: "JadeBaraMichardiere", pswd: "4hD7lkP#"
+  },
+];
+
 function Memberlogin(){
-  document.getElementById("JSstatements").innerHTML = "You're logged in !"
+  var username = document.getElementById("usernameid").value;
+  var password = document.getElementById("passwordid").value;
+  if (username == userslist[0].usrname){
+    if (password == userslist[0].pswd){
+
+    }
+    else{
+      document.getElementById("JSstatements").innerHTML = "Your password isn't correct"
+    }
+  }else {
+    document.getElementById("JSstatements").innerHTML = "Your username isn't correct"
+  }
+  document.getElementById("JSstatements").innerHTML = "Hey! It's nice to see you again " + username + " !";
+  var LoginBody = document.getElementById('login-div');
+  LoginBody.style.width = '0';
+  LoginBody.style.marginLeft = '-500px';
 }
 function MemberSignUp(){
   document.getElementById("JSstatements").innerHTML = "You're signed up !"
