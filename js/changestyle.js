799b4ae99f00b58466135ef6556bf1b075f6aa6e
Centaurus-dj <53085471+Centaurus-dj@users.noreply.github.com>
2020-07-01 14:05:06 +0200

    Try PHP

---
+++ js/changestyle.js
@@ -0,0 +1,16 @@
+
+
+function TransformToDark(){
+  var MainBody = document.getElementById("body");
+  var Links = document.getElementsByTagName('a');
+  //We now transform their background and their color
+  MainBody.style.background = "#323232";
+  MainBody.style.color = "#ffffff";
+}
+function TransformToLight(){
+  var MainBody = document.getElementById("body");
+  var Links = document.getElementsByTagName('a');
+  //We now transform their background and their color
+  MainBody.style.background = "#ffffff";
+  MainBody.style.color = "#000000";
+}
---
+++ js/script.js
@@ -0,0 +1,6 @@
+function Memberlogin(){
+  document.getElementById("JSstatements").innerHTML = "You're logged in !"
+}
+function MemberSignUp(){
+  document.getElementById("JSstatements").innerHTML = "You're signed up !"
+}
